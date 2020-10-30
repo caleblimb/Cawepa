4da2e1538c5e1a855cb1efa4e3d882304ddd57df
pDallastra <52924113+pDallastra@users.noreply.github.com>
2020-10-30 08:26:54 -0300

    Getting started on Sound Effects

--- src/game/sounds.py
+++ src/game/sounds.py
@@ -2,3 +2,7 @@
 This is where all of the sound effects and songs are declared.
 '''
 import pygame
+pygame.init()
+
+class Sounds():
+    standard_step_sound = pygame.mixer.Sound('res/sounds/Dirt_Jogging.mp3')
